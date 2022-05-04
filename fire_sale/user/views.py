2913a86e3e746a83eb54a99fbbfa8ab728fd98c8
Jonsteinar <jonss20@ru.is>
2022-05-04 18:50:13 +0000

    requirements.txt er fært, bætti við sma koða f item

--- fire_sale/user/views.py
+++ fire_sale/user/views.py
@@ -3,5 +3,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+
 def index(request):
+    #if 'search_filter' in request.GET:
+        #search_filter = request.GET('search_filter')
+        #item = [ {
+         #   'id': x.id,
+        #    'name': x.name,
+        #    'description': x.description
+        #} for x in Item.objects.filter(name__icontains=search_filter) ]
     return render(request, 'user/index.html')
+
+#def get_item_by_id(request, id):
+    #return render(request, 'item/item_details.html', {
+    #    'item': get_object_or_404(Item, pk=id)
+    #})
