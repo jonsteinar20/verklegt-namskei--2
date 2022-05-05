22819ccbe6188cd3a4835428e7d4e36542c9eb0a
Jonsteinar <jonss20@ru.is>
2022-05-05 13:06:29 +0000

    models fyrir user og item komið

--- fire_sale/item/models.py
+++ fire_sale/item/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class ItemCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True)
+    category = models.ForeignKey(ItemCategory, on_delete=models.CASCADE)
+    price = models.FloatField()
+    on_sale = models.BooleanField()
+
+class ItemImage(models.Model):
+    image = models.CharField(max_length=9999)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
