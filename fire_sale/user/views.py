4f09c05823b5b122befe069b7e9340b997692ceb
Jonsteinar <jonss20@ru.is>
2022-05-11 13:39:15 +0000

    profile ready

--- fire_sale/user/views.py
+++ fire_sale/user/views.py
@@ -1,14 +1,8 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-# Create your views here.
 from user.models import Profile
 from user.forms.profile_form import ProfileForm
 from user.forms.signup_form import SignUpForm
-
-
-def index(request):
-    return render(request, 'user/index.html')
 
 
 #def register(request):
@@ -37,7 +31,7 @@ def register(request):
 
 
 def profile(request):
-    profile = Profile.objects.get(user=request.user).first()
+    profile = Profile.objects.filter(user=request.user).first()
     if request.method == 'POST':
         form = ProfileForm(instance=profile, data=request.POST)
         if form.is_valid():
