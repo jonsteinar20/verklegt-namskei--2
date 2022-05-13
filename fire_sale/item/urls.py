242e16815f5d3a181318369c44261b90c628ac26
Jonsteinar <jonss20@ru.is>
2022-05-13 13:24:55 +0000

    categories

--- fire_sale/item/urls.py
+++ fire_sale/item/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('<int:id>', views.get_item_by_id, name="item_details"),
     path('create_item', views.create_item, name="create_item"),
     path('<int:item_id>/make_bid', views.make_bid, name="make_bid"),
-    #path('', views.index, name="item-categories")
+    #path('categories', views.categories, name="categories")
 ]
