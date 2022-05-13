26ac2eb484a4adbb1ba3d5f39989e994cb2d83b0
unnuroskg <unnurg19@ru.is>
2022-05-13 14:42:27 +0000

    no changes

--- fire_sale/contact_info/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class ContactInfo(models.Model):
-    full_name = models.CharField(max_length=999)
-    address = models.CharField(max_length=999)
-    city = models.CharField(max_length=999)
-    country = models.CharField(max_length=999)
-    postal_code = models.IntegerField()
